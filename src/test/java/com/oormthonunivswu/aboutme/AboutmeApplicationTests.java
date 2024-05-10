f667f5dbfd63c1cde44af40bb6b91b72839ab7ee
daeun-han <111632494+daeun-han@users.noreply.github.com>
2024-05-10 16:16:42 +0900

    feat: build project

---
+++ src/test/java/com/oormthonunivswu/aboutme/AboutmeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oormthonunivswu.aboutme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AboutmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
