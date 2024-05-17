a59cdda4f79fff3f8b7923a29d759b10daa070f2
daeunhan <daeunhan0723@gmail.com>
2024-05-17 20:21:11 +0900

    fix: join & login json type

--- src/main/java/com/oormthonunivswu/aboutme/Controller/UserController.java
+++ src/main/java/com/oormthonunivswu/aboutme/Controller/UserController.java
@@ -5,6 +5,8 @@ import com.oormthonunivswu.aboutme.Dto.LoginRequestDto;
 import com.oormthonunivswu.aboutme.Entity.User;
 import com.oormthonunivswu.aboutme.Service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
@@ -20,14 +22,24 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/api/join")
-    public String join(@RequestBody JoinRequestDto joinRequestDto) {
-
-        return userService.join(joinRequestDto);
+    public ResponseEntity<Object> join(@RequestBody JoinRequestDto joinRequestDto) {
+        String response = userService.join(joinRequestDto);
+        if (response.equals("회원가입 성공")) {
+            return ResponseEntity.status(HttpStatus.OK).body(Map.of("message", "회원가입 성공"));
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Map.of("message", response));
+        }
     }
 
     @PostMapping("/api/login")
-    public String login(@RequestBody LoginRequestDto loginRequestDto) {
-        return userService.login(loginRequestDto);
+    public ResponseEntity<Object> login(@RequestBody LoginRequestDto loginRequestDto) {
+        String response = userService.login(loginRequestDto);
+        if (response.startsWith("로그인 성공")) {
+            String jwtToken = response.replace("로그인 성공 ", "");
+            return ResponseEntity.status(HttpStatus.OK).body(Map.of("message", "로그인 성공", "token", jwtToken));
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(Map.of("message", "로그인 실패"));
+        }
     }
 
 
