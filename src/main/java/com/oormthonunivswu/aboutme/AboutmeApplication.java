c4122714c8f5adb973a2369832368165a8530df3
daeunhan <daeunhan0723@gmail.com>
2024-05-18 22:19:53 +0900

    fix: port

--- src/main/java/com/oormthonunivswu/aboutme/AboutmeApplication.java
+++ src/main/java/com/oormthonunivswu/aboutme/AboutmeApplication.java
@@ -2,14 +2,12 @@ package com.oormthonunivswu.aboutme;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication
 public class AboutmeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication app = new SpringApplication(AboutmeApplication.class);
-		app.setAdditionalProfiles("dev");
-		app.run(args);
+		SpringApplication.run(AboutmeApplication.class, args);
 	}
+
 }
