0c8547fd991c18ff40f18e05a500ccd3e5041f90
daeunhan <daeunhan0723@gmail.com>
2024-05-18 22:05:27 +0900

    fix: db url error

--- src/main/java/com/oormthonunivswu/aboutme/AboutmeApplication.java
+++ src/main/java/com/oormthonunivswu/aboutme/AboutmeApplication.java
@@ -2,8 +2,9 @@ package com.oormthonunivswu.aboutme;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class AboutmeApplication {
 
 	public static void main(String[] args) {
