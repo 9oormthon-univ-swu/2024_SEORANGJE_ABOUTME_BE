a6c9417b24bbcf9c6a115fe9d766dd559c418be0
hyojeongchoi <63584457+hyojeongchoi@users.noreply.github.com>
2024-05-18 16:17:58 +0900

    [feat] MyImageSubject API

--- src/main/java/com/oormthonunivswu/aboutme/controller/MyImageSubjectController.java
+++ src/main/java/com/oormthonunivswu/aboutme/controller/MyImageSubjectController.java
@@ -1,8 +1,9 @@
-package com.oormthonunivswu.aboutme.MyImageSubject;
+package com.oormthonunivswu.aboutme.controller;
+import com.oormthonunivswu.aboutme.dto.MyImageSubjectDTO;
+import com.oormthonunivswu.aboutme.service.MyImageSubjectService;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,8 +19,8 @@ public class MyImageSubjectController {
     private MyImageSubjectService myImageSubjectService;
 
     //MyImageSubject 반환 API
-    @GetMapping("/MyImageSubject/{userId}")
-    public MyImageSubjectDTO getSubjects(@PathVariable Long userId){
-        return myImageSubjectService.getMyImageSubject(userId);
+    @GetMapping("/MyImageSubject/{user_id}")
+    public MyImageSubjectDTO getSubjects(@PathVariable Long user_id){
+        return myImageSubjectService.getMyImageSubject(user_id);
     }
 }
