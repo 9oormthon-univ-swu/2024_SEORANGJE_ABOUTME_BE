ddca7abfd7251862a4b7c75afd7827b652f8ed53
daeunhan <daeunhan0723@gmail.com>
2024-05-20 21:28:14 +0900

    fix: cors

--- src/main/java/com/oormthonunivswu/aboutme/Config/WebConfig.java
+++ src/main/java/com/oormthonunivswu/aboutme/Config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.oormthonunivswu.aboutme.Config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,9 +11,17 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedOrigins("http://localhost:5173")
                 .allowedHeaders("*")
+                .allowedMethods(
+                        HttpMethod.GET.name(),
+                        HttpMethod.HEAD.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.PUT.name(),
+                        HttpMethod.DELETE.name()
+                )
+                //.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .exposedHeaders("Authorization", "RefreshToken", "X-AUTH-TOKEN")
                 .allowCredentials(true);
     }
 }
